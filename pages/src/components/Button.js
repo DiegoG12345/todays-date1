b95592a459a90541731897ccd9c4110a5915256b
DiegoG12345 <diego@opalcamera.com>
2022-06-07 20:37:04 -0700

    yes12

--- pages/src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import styles from '../../styles/Button.module.css'
-
-const Button = () => {
-    return <button className={styles.prettierButton} type="button">This is a button</button>
-
-}
-
-export default Button;
